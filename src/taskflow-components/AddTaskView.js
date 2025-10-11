b4d91012886e3260bd741d67f35267513ea23e00
Madelyn Mades <madesmadelyn@gmail.com>
2025-10-11 15:25:06 +0800

    task3: implement global task state and addTask/deleteTask service functions

--- src/taskflow-components/AddTaskView.js
+++ src/taskflow-components/AddTaskView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function AddTaskView() {
+export default function AddTaskView({ addtask }) {
   return (
     <div>
       <h2>Add Task (placeholder)</h2>
